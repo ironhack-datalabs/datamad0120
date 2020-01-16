94bf43e03b2052f8de1aa2b2baa1ce55fb48887d
lauravillalba <lvillalba@live.com>
2020-01-16 17:39:36 +0100

    Hastal el ejercicio 10

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -1,3 +1,4 @@
+'''
 #Example: 
 
 eggs = (1,3,8,3,2)
@@ -5,48 +6,52 @@ eggs = (1,3,8,3,2)
 my_listComprehension = [1/egg for egg in eggs]
 
 print(my_listComprehension)
-
+'''
 #Insert here the module/library import statements 
 import math
 
 
-
+'''
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-square= [math.sqrt(e) for e in range(2)]
+square= [e**2 for e in range(20)]
 print (square)
 
-'''
+
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+power_of_two=[2**e for e in range (50)]
+print (power_of_two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-
-
+sqrt=[math.sqrt(e) for e in range (100)]
+print(sqrt)
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+my_list=[-1*e for e in range (11)][::-1]
+print(my_list)
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
+odd=[e for e in range(1,100) if e%2!=0]
+print(odd)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+divisible_by_seven=[e for e in range(1,1000) if e%7==0]
+print(divisible_by_seven)
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
@@ -55,6 +60,9 @@ print (square)
 
 teststring = 'Find all of the words in a string that are monosyllabic'
 
+non_vowels=[e for e in teststring if e not in ["a" ,"e" , "i" , "o" , "u"]]
+''.join(non_vowels)
+print(''.join(non_vowels))
 
 
 
@@ -62,23 +70,33 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+sentence= 'The Quick Brown Fox Jumped Over The Lazy Dog'
 
+capital_letters=[e for e in list(sentence) if (e==e.capitalize() and e!=" ") ]
+print (capital_letters)
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
+sentence= 'The quick brown fox jumped over the lazy dog'
+consonants=[e for e in sentence.lower() if (e not in ["a" ,"e" , "i" , "o" , "u"] and e!=" ")]
+print (consonants)
 
-
-
-
+'''
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+import os
+files=[]
+with os.scandir('/home/laura/Escritorio/Ironhack_data/datamad0120/module-1/lab-errhand_listcomp') as entries:
+    for file in entries:
+        files.append(file.name)
+    print(files)
 
-
+'''
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
