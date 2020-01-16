517754a8c7391d1c807aa376b527e6b7561a38e0
Ricardoafsp <rafonsospinola@gmail.com>
2020-01-16 20:42:37 +0100

    Resuelto el ejercicio, me falto el bonus

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -8,89 +8,93 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
-
+import math
+import os
+import random
+import sys
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+square = [e**2 for e in list(range(0,20))]
+print(square)
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+power_of_two = [2**e for e in list(range(50))]
+print(power_of_two)
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-
+sqrt = [math.sqrt(e) for e in list(range(0,100))]
+print(sqrt)
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+my_list = [a for a in range(-10,1)]
+print(my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
-
+odds = [e for e in list(range(0,100)) if e%2==1]
+print(odds)
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+divisible_by_seven = [e for e in list(range(0,1000)) if e%7==0]
+print(divisible_by_seven)
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
 # You can use the following test string but feel free to modify at your convenience
 
 teststring = 'Find all of the words in a string that are monosyllabic'
-
-
-
+non_vowels = [e for e in teststring if e not in "aeiou"]
+print(non_vowels)
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
-
-
+sentence = "The Quick Brown Fox Jumped Over The Lazy Dog"
+capital_letters = [e for e in sentence if e.isupper()]
+print(capital_letters)
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
-
-
-
+consonants = [e for e in sentence if e not in "aeiou"]
+print(consonants)
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
-
+files = [e for e in os.listdir("/Users/ricardoafonsospinola/datamad0120-1/module-1/lab-errhand_listcomp/your-code")]
+print(files)
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
-
+random_lists = [random.sample(range(100),k=10) for e in range(4)]
+print(random_lists)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
-
-
+flatten_list = [a for b in [[e for e in k] for k in list_of_lists] for a in b]
+print(flatten_list)
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -98,15 +102,17 @@ list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', '20'], \
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
-
-
-
+floats = [float(x) for list in list_of_lists for x in list]
+print(floats)
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
-
 for i in ['a','b','c']:
-    print i**2
+  try:
+    print(i**2)
+  except TypeError as e:
+    print(e)
+    print("Vuelve a intentarlo")
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
@@ -115,53 +121,75 @@ for i in ['a','b','c']:
 
 x = 5
 y = 0
-
-z = x/y
-
-
-
+try:
+    z = x/y
+except ZeroDivisionError as e:
+    print(e)
+finally:
+    print("All Done")
 
 #16. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
 abc=[10,20,20]
-print(abc[3])
+try:
+    print(abc[3])
+except IndexError as e:
+    print(e)
+    print("La lista indexada está fuera de rango")
+
 
 
 #17. Handle at least two kind of different exceptions when dividing a couple of numbers provided by the user. 
 # Hint: take a look on python input function. 
 # Check in provided resources the type of error you may use. 
 
-
+while True:
+  try:
+    x = int(input("Introduce un número entero: "))
+    y = int(input("Introduce un número entero: "))
+    suma = x/y
+    print(suma)
+  except ValueError:
+    print("Introduce números enteros")
 
 
 #18. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
-f = open('testfile','r')
-f.write('Test write this')
-
-
-
+try:
+  f = open('testfile','r')
+  f.write('Test write this')
+except FileNotFoundError as e:
+  print(e)
+  print("No se encuentra el directorio")
 
 #19. Handle the exceptions that can be thrown by the code below using try and except blocks. 
 #Hint: the file could not exist and the data could not be convertable to int
 
-fp = open('myfile.txt')
+try:
+    fp = open('myfile.txt')
+except FileNotFoundError as e:
+    print("No encuentra el directorio")
+except NameError as d:
+    print("Nombre f no está definido")
     line = f.readline()
     i = int(s.strip())
-
-
-
 
 #20. The following function can only run on a Linux system. 
 # The assert in this function will throw an exception if you call it on an operating system other than Linux. 
 # Handle this exception using try and except blocks. 
 # You will probably need to import sys 
 
-def linux_interaction():
-    assert ('linux' in sys.platform), "Function can only run on Linux systems."
-    print('Doing something.')
+def linux_interaction():    
+    try:
+        assert ('linux' in sys.platform), "Function can only run on Linux systems."
+        print('Doing something.')
+    except AssertionError as e:
+        print(e)
+        print("Esta función solo corre en Linux")
+linux_interaction()
+
 
 
 # Bonus Questions:
