440a14d3da5a307d4326794c93fe45e68fe4591d
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-01-16 22:53:34 +0100

    No termino bonus porque no lo entiendo bien y... es muy tarde :( :(

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -9,7 +9,7 @@ my_listComprehension = [1/egg for egg in eggs]
 print(my_listComprehension)
 
 #Insert here the module/library import statements 
-import math
+import math 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 cuadrados = [i**2 for i in range(1,20)]
