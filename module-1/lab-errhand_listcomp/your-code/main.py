cfb6f009727090ae467c9def03995915e769d149
x22a <xabiergdvz@gmail.com>
2020-01-16 17:56:25 +0100

    Checkpoint

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -8,66 +8,79 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
+import math
+import os
+import random
 
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
+square = [e**2 for e in range(1,21)]
+print(square)
 
 
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
-
+power_of_two = [2**e for e in range(1,51)]
+print(power_of_two)
 
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
-
+sqrt = [math.sqrt(e) for e in range (1,101)]
+print(sqrt)
 
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
-
+my_list = [e for e in range(-10, 1, 1)]
+print(my_list)
 
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
-
+odds = [e for e in range(1,101) if e % 2 == 0]
+print(odds)
 
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
-
+divisible_by_seven = [e for e in range(1, 1001) if e % 7 == 0]
+print(divisible_by_seven)
 
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
 # You can use the following test string but feel free to modify at your convenience
-
+#non_vowels = [chr(e) for e in range(97,123) if e not in [97,101,105,111,117]]
 teststring = 'Find all of the words in a string that are monosyllabic'
-
+non_vowels = ''.join([e for e in teststring if e.lower() not in ['a','e','i','o','u']])
+print(non_vowels)
 
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
-
-
+sentence = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+capital_letters = [e for e in sentence if e.isupper()]
+print(capital_letters)
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
-
+sentence = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+consonants = ''.join([e for e in sentence if e.lower() not in ['a','e','i','o','u',' ']])
+print(consonants)
 
 
 
@@ -75,20 +88,22 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
+files = os.listdir('/Users/x/datamad0120/')
 
-
+print(files)
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
-
-
+random_lists = [[random.randrange(0, 100) for e in range(10)] for f in range(4)]
+print(random_lists)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
+flatten_list = [e for f in list_of_lists for e in f]
 
 
 
@@ -99,15 +114,19 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
-
+floats = [[float(f) for f in e] for e in list_of_lists]
+print(floats)
 
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
 
 for i in ['a','b','c']:
-    print i**2
-
+    try: 
+        print(i**2)
+    except Exception:
+        print('No se puede elevar al cuadrado un char')
+        
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
 #Then use a finally block to print 'All Done.'
