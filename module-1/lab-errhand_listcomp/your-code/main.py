f6c77eeb92606946b35c1ee90c7651a1556dceb8
Irene Lopez <irene.lopezs@protonmail.com>
2020-01-16 17:20:01 +0100

    Hasta el ejercicio 11.

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -8,45 +8,53 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
-
+import statements
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+square = [i**2 for i in range(0,20)]
 
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+power_of_two = [2**i for i in range(0,50)]
+print(power_of_two)
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
+import math
 
+sqrt = [math.sqrt(i) for i in range(0,100)]
+
+print(sqrt)
 
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+my_list = [i for i in reversed(range(0,-11,-1))]
 
-
+print(my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
+odds = [i for i in range(1,101) if i % 2 == 0]
 
-
+print(odds)
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+divisible_by_seven = [i for i in range(1,1001) if i % 7 == 0]
 
-
+print(divisible_by_seven)
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
@@ -55,12 +63,20 @@ print(my_listComprehension)
 teststring = 'Find all of the words in a string that are monosyllabic'
 
 
+non_vowels = ''.join([i for i in teststring.lower() if i not in ['a','b','c', 'd']])
+print(non_vowels)
+
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+testString2 = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+
+capital_letters = ''.join([i for i in testString2 if not i.isupper()])
+
+print(capital_letters)
 
 
 
@@ -68,7 +84,11 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
+testString3 = 'The quick brown fox jumped over the lazy dog'
 
+consonants = [i for i in testString3 if i != ' ']
+
+print(consonants)
 
 
 
@@ -76,11 +96,19 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+#ME FALTA QUITAR LOS ARCHIVOS
+files_and_folders = os.listdir('/home/irene/datamad0120')
+folders = 
+print(folders)
 
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
+
+import random
+
+random_lists = [random.randrange(0,100) for i in range(10)]
 
 
 
@@ -106,7 +134,7 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 
 
 for i in ['a','b','c']:
-    print i**2
+    print(i**2)
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
