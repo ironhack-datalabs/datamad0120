0560bdf916fba9bf9b2a19eb1f1118269bb51c6c
Alex <correovidal@outlook.com>
2020-01-17 00:24:07 +0100

    Falta parte del bonus

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -19,15 +19,16 @@ import sys
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+square = [e**2 for e in range(20)]
 
-print([e**2 for e in range(20)])
+print("Ej1 \n", square)
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
 power_of_two = [2**e for e in range(50)]
 
-print(power_of_two)
+print("Ej2 \n", power_of_two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
@@ -36,7 +37,7 @@ print(power_of_two)
 
 sqrt = [math.sqrt(element) for element in range(100)]
 
-print(sqrt)
+print("Ej3 \n", sqrt)
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
@@ -44,14 +45,14 @@ print(sqrt)
 
 my_list = [element for element in range(-10,1,1)]
 
-print(my_list)
+print("Ej4 \n", my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
 odds = [element for element in range(1,100) if element % 2 != 0]
 
-print(odds)
+print("Ej5 \n", odds)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
@@ -59,7 +60,7 @@ print(odds)
 
 divisible_by_seven = [element for element in range(1,1000) if element % 7 == 0]
 
-print(divisible_by_seven)
+print("Ej6 \n", divisible_by_seven)
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
@@ -69,7 +70,7 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 non_vowels = [element for element in teststring if element in "aeiou"]
 
-print(non_vowels)
+print("Ej7 \n", non_vowels)
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
@@ -81,7 +82,7 @@ sentence = "The Quick Brown Fox Jumped Over The Lazy Dog"
 
 capital_letters = [e for e in sentence if e.isupper()]
 
-print(capital_letters)
+print("Ej8 \n", capital_letters)
 
 
 
@@ -93,14 +94,22 @@ sentence = "The quick brown fox jumped over the lazy dog"
 
 consonants = [word for word in sentence if word not in "aeiou" and word != " "]
 
-print(consonants)
+print("Ej9 \n", consonants)
 
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
-print(os.listdir("/home/alex/Documentos/Repositorios/datamad0120/"))
+files = [element for element in os.listdir("/home/alex/Documentos/Repositorios/datamad0120/")]
+
+print("Ej10 \n", files)
+
+
+
+#¿Este código funcionaría?
+#print([os.listdir(e) for element in os.listdir("/home/alex/Documentos/Repositorios/datamad0120/") for e in os.path.abspath(element) if os.path.exists(e)])
+
 
 
 
@@ -111,7 +120,7 @@ print(os.listdir("/home/alex/Documentos/Repositorios/datamad0120/"))
 
 random_lists = [random.randint(0,100) for e in range(10)], [random.randint(0,100) for e in range(10)], [random.randint(0,100) for e in range(10)], [random.randint(0,100) for e in range(10)]
 
-print(random_lists)
+print("Ej11 \n", random_lists)
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
@@ -122,7 +131,7 @@ list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 flatten_list = [element for list in list_of_lists for element in list]
 
 
-print(flatten_list)
+print("Ej12 \n", flatten_list)
 
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
@@ -135,7 +144,7 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 
 floats = [float(element) for list in list_of_lists for element in list]
 
-print(floats)
+print("Ej13 \n", floats)
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
@@ -219,6 +228,10 @@ except FileNotFoundError as e:
 
     print("18. Error capturado: %s" %(e))
 
+except NameError as e:
+
+    print("18. Error capturado: %s" %(e))
+
 
 #19. Handle the exceptions that can be thrown by the code below using try and except blocks. 
 #Hint: the file could not exist and the data could not be convertable to int
@@ -232,6 +245,19 @@ except FileNotFoundError:
 
     print("19. Error capturado: FileNotFoundError")
 
+except NameError as e:
+
+    print("19. Error capturado: %s" %(e))    
+
+except ValueError as e:
+
+    print("19. Error capturado: %s" %(e))
+
+except AttributeError as e: 
+
+    print("19. Error capturado: %s" %(e))
+
+
 
 
 #20. The following function can only run on a Linux system. 
@@ -241,15 +267,17 @@ except FileNotFoundError:
 
 
 def linux_interaction():
-    assert ('linux' in sys.platform), "Function can only run on Linux systems."
-    print('Doing something.')
+
+    try:
+
+        assert ('linux' in sys.platform), "Function can only run on Linux systems."
+        print('Doing something.')
+
+    except AssertionError:
+
+        raise Exception("Closed source operating system detected")
 
 linux_interaction()
-
-#IT WORKS, LINUX POWAAAH!!
-
-
-
 
 
 
@@ -264,7 +292,26 @@ linux_interaction()
 # Hint: we need to continually keep checking until we get an integer.
 # Use a while loop with a try,except, else block to account for incorrect inputs.
 
+def question():
 
+    count = 0
+
+    while True:
+
+        try:
+
+            ask = int(input("Introduce número"))**2
+
+            return ("Has contestado %d tras %d intentos" %(ask, count))
+
+        except ValueError:
+
+            print("Intentalo de nuevo")
+
+            count += 1
+
+              
+print(question())
 
 
 
@@ -273,13 +320,20 @@ linux_interaction()
 # Use results as the name of the list 
 
 
+#Ejercicio patrocinado por "Fuga de Maroto"
+
+
+#¿Este ejercicio se resuelve con una dict comprehension?
+
 
 
 # 23. Define a customised exception to handle not accepted values. 
 # You have the following user inputs and the Num_of_sections can not be less than 2.
 # Hint: Create a class derived from the pre-defined Exception class in Python
 
-# Total_Marks = int(input("Enter Total Marks Scored: ")) 
-# Num_of_Sections = int(input("Enter Num of Sections: "))
+Total_Marks = int(input("Enter Total Marks Scored: ")) 
+Num_of_Sections = int(input("Enter Num of Sections: "))
+
+
 
 
