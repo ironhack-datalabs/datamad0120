3c685d1089dbffdd17466cb5d0b8ec61bb6d46c1
patricp <publizistik.try@gmail.com>
2020-01-16 17:52:15 +0100

    Saving

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -8,44 +8,59 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
-
+import math
+import os
+import random
+from random import sample
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+square = [n ** 2 for n in range(20)]
 
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+power_of_two = [2 ** n for n in range(51)]
 
+print(two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
+sqrt = [math.sqrt(n) for n in range(100)]
 
+print(sqrt)
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+my_list = [-n for n in range(11)]
+my_list.sort()
+print(my_list)
 
+my_list_2 = [n for n in range(-10,1)]
+print(my_list_2)
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
+odds = [n for n in range(1,101) if n % 2 != 0]
+print(odds)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+divisible_by_seven = [n for n in range(1,1001) if n % 7 == 0]
+print(divisible_by_seven)
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
@@ -53,7 +68,12 @@ print(my_listComprehension)
 # You can use the following test string but feel free to modify at your convenience
 
 teststring = 'Find all of the words in a string that are monosyllabic'
+non_vowels = [l for l in teststring if l.lower() not in ("a", "e", "i", "o", "u")]
 
+
+print(non_vowels)
+
+"".join(non_vowels)
 
 
 
@@ -61,14 +81,23 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+sentence = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+capital_letters = [l for l in sentence if l.isupper()] 
 
+print(capital_letters)
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
+teststring_2 = 'The quick brown fox jumped over the lazy dog'
+consonants = [l for l in teststring_2 if l.lower() not in ("a", "e", "i", "o", "u", " ")]
 
+
+print(consonants)
+
+"".join(consonants)
 
 
 
@@ -76,13 +105,15 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+files = [x for x in os.listdir("/Users/TryAgain/datamad0120/")]
 
+print(files)
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
-
+random_lists = [[n for n in random.sample(range(101), 10)] for x in range(4)]
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
@@ -90,6 +121,9 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 
+flatten_list = [n for l in list_of_lists for n in l]
+
+print(flatten_list)
 
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
@@ -100,13 +134,17 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['100', '100', '100', '100']]
 
 
-
+floats = [[float(n) for n in l] for l in list_of_lists]
+print(floats)
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
 
 for i in ['a','b','c']:
-    print i**2
+  try:
+    print(i**2)
+  except Exception:
+    print("It's not a number")
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
@@ -118,6 +156,11 @@ y = 0
 
 z = x/y
 
+try:
+  z = x/y
+except ZeroDivisionError:
+  print("Not divisible by zero")
+  print("All done")
 
 
 
