c8efa768f5134e032b8864635234b0b072af813e
sowe01 <sowe01@gmail.com>
2020-01-16 20:46:07 +0100

    ejercicio dia 4

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -8,43 +8,55 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
+import math
+import os
+import random
+import sys
 
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
 
-
+list = range(0,20)
+square=([x**2 for x in list])
+print(square) 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+power_of_two=[2**i for i in range(50)]
 
+print (power_of_two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-
+sqrt=[math.sqrt(i) for i in range(100)]
+print(sqrt)
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
-
+my_list=[i for i in range(-10,1,1)]
+print(my_list)
 
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
-
+odds=[i for i in range (100) if i%2==0]
+print(odds)
 
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+divisible_by_seven=[i for i in range (1000) if i%7==0]
+print(divisible_by_seven)
 
 
 
@@ -54,21 +66,28 @@ print(my_listComprehension)
 
 teststring = 'Find all of the words in a string that are monosyllabic'
 
-
+vocales = {"a", "e", "i", "o", "u", "A", "E", "I", "O","U"}
+non_vowels= [nv for nv in teststring if nv not in vocales]
+print(non_vowels)
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
-
+sentence= 'The Quick Brown Fox Jumped Over The Lazy Dog'
+capital_letters=[s for s in sentence if s.isupper()]
+print(capital_letters)
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
-
+sentence='The quick brown fox jumped over the lazy dog'
+vocales = {"a", "e", "i", "o", "u", "A", "E", "I", "O","U"}
+consonants="".join(nv for nv in teststring if nv not in vocales)
+print(consonants)
 
 
 
@@ -77,11 +96,14 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Remember to use list comprehensions and to print your results.
 
 
-
+file=[i for i in os.listdir("/home/jose/datamad0120/module-1/lab-errhand_listcomp")]
+print (file)
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+random_list=[random.randint(0, 100) for x in range(1, 11)]
+print(random_list)
 
 
 
@@ -90,7 +112,9 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 
+flatten_list=[item for sublist in list_of_lists for item in sublist]
 
+print(flatten_list)
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -99,6 +123,8 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
+floats = [[float(i) for i  in sublist] for sublist in list_of_lists]
+print(floats)
 
 
 
@@ -107,6 +133,11 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 
 for i in ['a','b','c']:
     print i**2
+try:
+    print (i**2)
+   
+except TypeError:
+    print("no es un numero valido.  Try again...")
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
@@ -116,41 +147,61 @@ for i in ['a','b','c']:
 x = 5
 y = 0
 
-z = x/y
-
-
-
+try:
+    z = x/y
+except SyntaxError as e:
+    print(e)
+finally:
+    print("All Done")
 
 #16. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
 abc=[10,20,20]
-print(abc[3])
-
+try:
+    print(abc[3])
+except IndexError as e:
+    print("el indice de la lista esta fuera de rango")
 
 #17. Handle at least two kind of different exceptions when dividing a couple of numbers provided by the user. 
 # Hint: take a look on python input function. 
 # Check in provided resources the type of error you may use. 
-
+while True:
+     try:
+        x = int(input("introduce un numero: "))
+        y = int(input("introduce otro numero: "))
+        div=x/y
+        print(div)
+        break
+     except ValueError:
+         print("Ha introducido un numero incorrecto, vuelve a intentarlo...")
 
 
 
 #18. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
-f = open('testfile','r')
-f.write('Test write this')
-
-
+try:
+    f = open('testfile','r')
+    f.write('Test write this')
+except FileNotFoundError:
+    print("introduce el directorio completo, no solo el nombre del archivo")
 
 
 #19. Handle the exceptions that can be thrown by the code below using try and except blocks. 
 #Hint: the file could not exist and the data could not be convertable to int
 
-fp = open('myfile.txt')
-    line = f.readline()
+try:
+    f = open('myfile.txt')
+    s = f.readline()
     i = int(s.strip())
-
+except OSError as err:
+    print("OS error: {0}".format(err))
+except ValueError:
+    print("Could not convert data to an integer.")
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
 
 
 
@@ -160,9 +211,12 @@ fp = open('myfile.txt')
 # You will probably need to import sys 
 
 def linux_interaction():
-    assert ('linux' in sys.platform), "Function can only run on Linux systems."
-    print('Doing something.')
+    try:
+        assert ('linux' in sys.platform), "Function can only run on Linux systems."
+        print('Doing something.')
 
+    except Assertionerror:
+        print("asa")
 
 # Bonus Questions:
 
