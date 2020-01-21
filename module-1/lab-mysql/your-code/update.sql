57b85c650b234527270d672bcb028eaad8759dca
Mariló Molina Candela <hec.lopezvizcaino@gmail.com>
2020-01-21 20:18:26 +0100

    delete & update

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,10 @@
+/*
+SELECT * FROM Salespersons
+*/
+UPDATE `lab_mysql`.`Salespersons` SET `Store` = 'Miami' WHERE (`idSalespersons` = '4');
+/*
+SELECT * FROM Customers
+*/
+UPDATE `lab_mysql`.`Customers` SET `Email` = 'ppicasso@gmail.com' WHERE (`idCustomers` = '0');
+UPDATE `lab_mysql`.`Customers` SET `Email` = '	lincoln@us.gov' WHERE (`idCustomers` = '1');
+UPDATE `lab_mysql`.`Customers` SET `Email` = 'hello@napoleon.me' WHERE (`idCustomers` = '2');
