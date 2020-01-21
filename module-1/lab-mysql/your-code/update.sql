7e0d700d494594b1502d2a0aae90416fe7903cad
patricp <publizistik.try@gmail.com>
2020-01-21 20:07:23 +0100

    Saving final

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,4 @@
+UPDATE `lab_mysql`.`salespersons` SET `store` = 'Miami' WHERE (`idsalespersons` = '5') and (`IDstaff` = '5');
+UPDATE `lab_mysql`.`customers` SET `email`= 'ppicasso@gmail.com' WHERE (`idcustomers` = '1') AND (`customerid`= '10001');
+UPDATE `lab_mysql`.`customers` SET `email`= 'lincoln@us.gov' WHERE (`idcustomers` = '2') AND (`customerid`= '20001');
+UPDATE `lab_mysql`.`customers` SET `email`= 'hello@napoleon.me' WHERE (`idcustomers` = '3') AND (`customerid`= '30001');
