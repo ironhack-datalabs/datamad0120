d1b7ee2372f85c1855de7e92d98bce8282a6b75e
lauravillalba <lvillalba@live.com>
2020-01-21 22:03:21 +0100

    update sql

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,11 @@
+UPDATE Customers
+	SET Email = "ppicasso@gmail.com"
+	WHERE idCustomers = "1";
+
+UPDATE Customers
+	SET Email = "lincoln@us.gov"
+    WHERE idCustomers = "2";
+
+UPDATE Customers
+	SET Email = "hello@napoleon.me"
+    WHERE idCustomers = "3";
