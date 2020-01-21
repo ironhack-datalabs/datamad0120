1d02b9da1ec7be10d47c65c2df0953e8b2b2e3f6
Shurlena <marlenapolak@gmail.com>
2020-01-22 00:25:54 +0100

    update.sql

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE Salespersons
+    SET Store = "Miami"
+    WHERE ID_salespersons = 4;
+
+UPDATE Customers
+    SET Email = "ppicasso@gmail.com"
+    WHERE ID_customers = 0;
+
+UPDATE Customers
+    SET Email = "lincoln@us.gov"
+    WHERE ID_customers = 1;
+
+UPDATE Customers
+    SET Email = "hello@napoleon.me"
+    WHERE ID_customers = 2;
