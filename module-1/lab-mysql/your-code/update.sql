98ab4354947a1f81824c63d6261a5f8bf2f13d41
Maria Roig <mariaroigporta@gmail.com>
2020-01-22 05:58:26 +0100

    Solved Ej Update

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE salesforce
+    SET store = "Miami"
+    WHERE id_auto_sales = 4;
+
+UPDATE Customers
+    SET email = "ppicasso@gmail.com"
+    WHERE idCustomers = 0;
+
+UPDATE Customers
+    SET email="lincoln@us.gov"
+    WHERE idCustomers=1;
+
+UPDATE Customers
+    SET email="hello@napoleon.me"
+    WHERE idCustomers=2;
