a3e2e3aeb3464bbb518ceac91d351239166b1f24
Fernando Costa <fernandocosta@Fernandos-MacBook-Pro.local>
2020-01-22 20:41:05 +0100

    Challenges

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,3 +1,7 @@
+lab_mysql_select
+
+#CHALLENGE 1
+
 SELECT authors.au_id as "AUTHOR ID", 
 authors.au_lname as "LAST NAME", 
 authors.au_fname as "FIRST NAME", 
@@ -7,4 +11,48 @@ publishers.pub_name as "PUBLISHER"
     INNER JOIN titleauthor
     ON authors.au_id = titleauthor.au_id
     INNER JOIN  titles ON titles.title_id = titleauthor.title_id
-    INNER JOIN publishers ON publishers.pub_id = titles.pub_id¡
+    INNER JOIN publishers ON publishers.pub_id = titles.pub_id
+
+# CHALLENGE 2
+
+SELECT authors.au_id as `AUTHOR ID`,
+authors.au_lname as "LAST NAME",
+authors.au_fname as "FIRST NAME",
+titles.title as "TITLE",
+publishers.pub_id as "PUBLISHER"
+    FROM authors
+    INNER JOIN titleauthor
+    ON authors.au_id = titleauthor.au_id
+    INNER JOIN titles ON titles.title_id = titleauthor.title_id
+    INNER JOIN publishers ON publishers.pub_id = titles.pub_id;
+
+
+#CHALLENGE 3
+
+SELECT authors.au_id as `AUTHOR ID`,
+authors.au_lname as "LAST NAME",
+authors.au_fname as "FIRST NAME",
+titles.title as "TITLE",
+SUM(sales.qty) as `Total`
+    FROM authors
+    INNER JOIN titleauthor
+    ON authors.au_id = titleauthor.au_id
+    INNER JOIN titles ON titles.title_id = titleauthor.title_id
+    INNER JOIN sales ON sales.title_id = titles.title_id
+    GROUP BY `AUTHOR ID` ORDER BY `Total` DESC
+    LIMIT 3;
+
+#CHALLENGE 4
+
+SELECT authors.au_id as `AUTHOR ID`,
+authors.au_lname as "LAST NAME",
+authors.au_fname as "FIRST NAME",
+titles.title as "TITLE",
+SUM(sales.qty) as `Total`
+    FROM authors
+    INNER JOIN titleauthor
+    ON authors.au_id = titleauthor.au_id
+    LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+    LEFT JOIN sales ON sales.title_id = titles.title_id
+	ORDER BY `Total` DESC
+    LIMIT 23;
