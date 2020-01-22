c29362c3ed43b442c6c999947c344c4f1800b6fe
vagabundoo <evillamorm@gmail.com>
2020-01-22 19:44:34 +0100

    challenge 2+3 done

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -22,6 +22,7 @@ FROM publications.authors as au
 -- Step 3
 
 SELECT au.au_id, 
+    t.title_id,
     round(SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) + t.advance * ta.royaltyper / 100, 2) AS moneypsale
 FROM publications.authors as au
 	LEFT JOIN titleauthor as ta ON au.au_id=ta.au_id
@@ -29,7 +30,36 @@ FROM publications.authors as au
     LEFT JOIN sales as s ON t.title_id=s.title_id
     GROUP BY au.au_id;
     
--- Challenge 2
+-- Challenge 2 & 3
+
+
+#DROP Temporary table mostProfTitles;
+
+CREATE temporary table mostProfTitles
+SELECT 
+	au.au_id,
+    t.title_id,
+	round(SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) + t.advance * ta.royaltyper / 100, 2) AS moneypsale
+FROM publications.authors as au
+	INNER JOIN titleauthor as ta ON au.au_id=ta.au_id
+    INNER JOIN titles as t On ta.title_id=t.title_id
+    INNER JOIN sales as s ON t.title_id=s.title_id
+    GROUP by au.au_id, t.title_id;
+
+#DROP Temporary table mostProfitingAuthors
+
+CREATE temporary table mostProfitingAuthors
+SELECT
+	au_id,
+    title_id,
+    moneypsale
+FROM mostProfTitles;
+
+SELECT * 
+FROM mostProfitingAuthors;
+
+    
+    
 
 
 		
