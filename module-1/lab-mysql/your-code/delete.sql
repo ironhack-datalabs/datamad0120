490cab7e351c143b9595f19e61634978d4687ee8
MdeMaic <miguel.ceinos@gmail.com>
2020-01-22 11:18:00 +0100

    con BONUS

---
+++ module-1/lab-mysql/your-code/delete.sql
@@ -0,0 +1,1 @@
+DELETE FROM `lab_mysql`.`Vehicles` WHERE `idVehicles`='4';
---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,5 @@
+UPDATE `lab_mysql`.`Salespersons` SET `staffStore`='Miami' WHERE `idSalespersons`='5';
+
+UPDATE `lab_mysql`.`Customers` SET `custMail`='ppicasso@gmail.com' WHERE `idCustomers`='7';
+UPDATE `lab_mysql`.`Customers` SET `custMail`='lincoln@us.gov' WHERE `idCustomers`='8';
+UPDATE `lab_mysql`.`Customers` SET `custMail`='hello@napoleon.me' WHERE `idCustomers`='9';
