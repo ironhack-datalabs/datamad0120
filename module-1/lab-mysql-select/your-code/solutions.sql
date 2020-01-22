77ced456ac258524161a1e81239e31fad3c7198d
Fernando Costa <fernandocosta@Fernandos-MacBook-Pro.local>
2020-01-22 17:19:47 +0100

    solutions

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,3 +1,19 @@
-lab-mysql-select 
+SELECT authors.au_id as "AUTHOR ID", 
+authors.au_lname as "LAST NAME", 
+authors.au_fname as "FIRST NAME", 
+titles.title as "TITLE", 
+publishers.pub_name as "PUBLISHER" 
+FROM authors LEFT JOIN titles
+ON authors.au_id = AUTHOR ID, authors.au_lname = LAST NAME, authors.au_fname = FIRST NAME, titles.title = TITLE
+FROM authors LEFT JOIN publishers 
+ON publishers.pub_name = PUB NAME
 
 
+SELECT authors.au_id as "AUTHOR ID", 
+authors.au_lname as "LAST NAME", 
+authors.au_fname as "FIRST NAME", 
+titles.title as "TITLE", 
+publishers.pub_name as "PUBLISHER" 
+FROM authors LEFT JOIN titles
+ON authors.au_id, authors.au_lname, authors.au_fname, titles.title
+LEFT JOIN publishers ON publishers.pub_name
