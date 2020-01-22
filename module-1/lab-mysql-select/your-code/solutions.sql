23852712f405567b593ef7ae11dda194b00231b1
Fernando Costa <fernandocosta@Fernandos-MBP.home>
2020-01-22 22:54:00 +0100

    Solutions

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -18,13 +18,14 @@ publishers.pub_name as "PUBLISHER"
 SELECT authors.au_id as `AUTHOR ID`,
 authors.au_lname as "LAST NAME",
 authors.au_fname as "FIRST NAME",
-titles.title as "TITLE",
-publishers.pub_id as "PUBLISHER"
+publishers.pub_name as "PUBLISHER",
+COUNT(titles.title) as "TITLE COUNT"
     FROM authors
-    INNER JOIN titleauthor
+    RIGHT JOIN titleauthor
     ON authors.au_id = titleauthor.au_id
-    INNER JOIN titles ON titles.title_id = titleauthor.title_id
-    INNER JOIN publishers ON publishers.pub_id = titles.pub_id;
+    LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+    LEFT JOIN publishers ON publishers.pub_id = titles.pub_id;
+    GROUP BY authors.au_id ORDER BY authors.au_id
 
 
 #CHALLENGE 3
@@ -35,9 +36,9 @@ authors.au_fname as "FIRST NAME",
 titles.title as "TITLE",
 SUM(sales.qty) as `Total`
     FROM authors
-    INNER JOIN titleauthor
+    LEFT JOIN titleauthor
     ON authors.au_id = titleauthor.au_id
-    INNER JOIN titles ON titles.title_id = titleauthor.title_id
+    LEFT JOIN titles ON titles.title_id = titleauthor.title_id
     INNER JOIN sales ON sales.title_id = titles.title_id
     GROUP BY `AUTHOR ID` ORDER BY `Total` DESC
     LIMIT 3;
