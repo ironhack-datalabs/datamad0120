910806136d459b3cc10fa3c7aeb67f754d2b2767
vagabundoo <evillamorm@gmail.com>
2020-01-22 17:51:29 +0100

    lab1 done except bonus

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,38 @@
+-- Challenge 1
+SELECT au.au_id, au.au_fname, au.au_lname, t.title, p.pub_name FROM publications.authors as au
+	LEFT JOIN titleauthor as ta ON au.au_id=ta.au_id
+    INNER JOIN titles as t On ta.title_id=t.title_id
+    LEFT JOIN publishers as p ON t.pub_id=p.pub_id
+    #WHERE t.title IS NOT Null;
+
+-- Chalenge 2
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+SELECT au.au_id, au.au_fname, au.au_lname, p.pub_name, count(t.title) as cnt FROM publications.authors as au
+	LEFT JOIN titleauthor as ta ON au.au_id=ta.au_id
+    INNER JOIN titles as t On ta.title_id=t.title_id
+    LEFT JOIN publishers as p ON t.pub_id=p.pub_id
+    #WHERE t.title IS NOT Null
+    group by au.au_id, p.pub_id
+    ORDER BY cnt DESC;
+    
+-- Challenge 3
+
+SELECT au.au_id, au.au_fname, au.au_lname, sum(s.qty) as sales FROM publications.authors as au
+	LEFT JOIN titleauthor as ta ON au.au_id=ta.au_id
+    INNER JOIN titles as t On ta.title_id=t.title_id
+    INNER JOIN sales as s ON t.title_id=s.title_id
+    group by au.au_id
+    ORDER BY sales DESC
+    limit 3;
+
+-- Challenge 4
+
+SELECT au.au_id, au.au_fname, au.au_lname, IFNULL(sum(s.qty),0) as sales FROM publications.authors as au
+	LEFT JOIN titleauthor as ta ON au.au_id=ta.au_id
+    LEFT JOIN titles as t On ta.title_id=t.title_id
+    LEFT JOIN sales as s ON t.title_id=s.title_id
+    group by au.au_id
+    ORDER BY sales DESC
+
+
