0ffa5e890595337b3ab72b9e88c9cd9879271e1d
Alex <correovidal@outlook.com>
2020-01-23 00:24:48 +0100

    Ejercicio en progreso V3

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,1 +1,56 @@
-asd
+/*
+CREATE TEMPORARY TABLE publications.Paso1
+
+SELECT
+	titles.title_id AS TitleID,
+	authors.au_id AS AuthorsID,
+    	titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 AS Sales_Royalties
+
+FROM authors
+    
+    INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
+    INNER JOIN titles ON titles.title_id=titleauthor.title_id
+    INNER JOIN sales ON sales.title_id=titles.title_id;
+*/    
+
+
+/*
+
+PD EXTRA --> PORQUE NO FUNCIONA DE ESTA MANERA, ¿NO ES IGUAL QUE INSERTAR UNA TABLA TEMPORAL ANTERIOR?
+
+CREATE TEMPORARY TABLE publications.Paso2
+
+SELECT 
+	AuthorsID,
+	TitleID,
+	SUM(Sales_Royalties) as Suma
+    
+FROM(
+	SELECT
+		titles.title_id AS TitleID,
+		authors.au_id AS AuthorsID,
+		titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 AS Sales_Royalties
+    FROM authors
+    
+    INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
+    INNER JOIN titles ON titles.title_id=titleauthor.title_id
+    INNER JOIN sales ON sales.title_id=titles.title_id;
+	
+) as Paso1
+GROUP BY AuthorsID, TitleID;
+
+*/
+
+/*
+CREATE TEMPORARY TABLE publications.Paso2
+
+SELECT 
+	AuthorsID,
+	TitleID,
+	SUM(Sales_Royalties) as Suma
+    
+FROM Paso1
+GROUP BY AuthorsID, TitleID;
+
+*/
+
