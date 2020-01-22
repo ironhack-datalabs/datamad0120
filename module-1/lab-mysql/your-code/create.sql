71ac00651104c646d6ad9893aeb34f004d2f9b22
fabianlojo <59798704+fabianlojo@users.noreply.github.com>
2020-01-22 04:10:28 +0100

    done

---
+++ module-1/lab-mysql/your-code/create.sql
@@ -0,0 +1,6 @@
+USE lab_mysql;
+CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20),
+       species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+       
+CREATE TABLE car (manufacturer VARCHAR(20), model VARCHAR(20),
+       year VARCHAR(20), color VARCHAR(10));
