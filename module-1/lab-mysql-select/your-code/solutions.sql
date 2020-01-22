86ef1f254fe12c093c610c42b48cf34e85b1bb63
Fernando Costa <fernandocosta@Fernandos-MacBook-Pro.local>
2020-01-22 18:20:35 +0100

    Challenge 1

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -3,17 +3,8 @@ authors.au_lname as "LAST NAME",
 authors.au_fname as "FIRST NAME", 
 titles.title as "TITLE", 
 publishers.pub_name as "PUBLISHER" 
-FROM authors LEFT JOIN titles
-ON authors.au_id = AUTHOR ID, authors.au_lname = LAST NAME, authors.au_fname = FIRST NAME, titles.title = TITLE
-FROM authors LEFT JOIN publishers 
-ON publishers.pub_name = PUB NAME
-
-
-SELECT authors.au_id as "AUTHOR ID", 
-authors.au_lname as "LAST NAME", 
-authors.au_fname as "FIRST NAME", 
-titles.title as "TITLE", 
-publishers.pub_name as "PUBLISHER" 
-FROM authors LEFT JOIN titles
-ON authors.au_id, authors.au_lname, authors.au_fname, titles.title
-LEFT JOIN publishers ON publishers.pub_name
+    FROM authors 
+    INNER JOIN titleauthor
+    ON authors.au_id = titleauthor.au_id
+    INNER JOIN  titles ON titles.title_id = titleauthor.title_id
+    INNER JOIN publishers ON publishers.pub_id = titles.pub_id¡
