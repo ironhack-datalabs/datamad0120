dc37636225551999031a6eceb73a133626b0f651
Cristina Camargo Reguera <cristina.cam.reg@gmail.com>
2020-01-22 19:41:39 +0100

    Challenge 3

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,43 @@
+CHALLENGE 1:
+/*PRIMERA PRUEBA*/
+/*SELECT authors.au_id as "AUTHOR ID", authors.au_lname as  "LAST NAME", authors.au_fname as "FIRST NAME", 
+p.title as "TITLE", p.pub_name as "PUBLISHER" 
+	FROM (SELECT t.title, t.pub_name, titleauthor.au_id 
+		FROM (SELECT title_id, pub_name, title 
+			FROM titles
+			LEFT JOIN publishers ON publishers.pub_id=titles.pub_id) as t
+        RIGHT JOIN titleauthor ON t.title_id=titleauthor.title_id) as p
+	LEFT JOIN authors ON p.au_id=authors.au_id;*/
+
+/*SEGUNDA PRUEBA*/
+
+SELECT authors.au_id as "AUTHOR ID", authors.au_lname as  "LAST NAME", authors.au_fname as "FIRST NAME", 
+titles.title as "TITLE", publishers.pub_name as "PUBLISHER"
+	FROM authors
+    INNER JOIN titleauthor ON authors.au_id=titleauthor.au_id
+    INNER JOIN titles ON titles.title_id=titleauthor.title_id
+    INNER JOIN publishers ON publishers.pub_id=titles.pub_id;
+
+CHALLENGE 2:
+
+SELECT authors.au_id as `AUTHOR ID`, authors.au_lname as  "LAST NAME", authors.au_fname as "FIRST NAME", publishers.pub_name as `PUBLISHER`,
+count(titles.title) as "TITLE"
+	FROM authors
+    INNER JOIN titleauthor ON authors.au_id=titleauthor.au_id
+    INNER JOIN titles ON titles.title_id=titleauthor.title_id
+    INNER JOIN publishers ON publishers.pub_id=titles.pub_id
+    GROUP BY `AUTHOR ID`, `PUBLISHER`;
+
+CHALLENGE 3:
+
+SELECT authors.au_id as `AUTHOR ID`, authors.au_lname as `LAST NAME`, authors.au_id as `FIRST NAME`, sum(sales.qty) as `TOTAL`
+FROM authors
+INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
+INNER JOIN sales on sales.title_id=titleauthor.title_id
+
+GROUP BY `AUTHOR ID`
+ORDER BY `TOTAL` DESC
+LIMIT 3;
+
+CHALLENGE 4:
+
