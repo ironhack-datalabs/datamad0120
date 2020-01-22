52bba6e3a4013270bae1be0fb9b7147984878f5f
eduman13 <eduman13@gmail.com>
2020-01-22 23:53:06 +0100

    Subimos una mejora

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,7 +1,7 @@
 select 
-    a.au_lname LAST_NAME, 
+    a.au_lname LAST_NAME,
     a.au_fname FIRST_NAME,
-    sum(distinct(t.price * t.royalty/100 * ta.royaltyper/100) + t.advance * ta.royaltyper/100) as TOTAL
+    sum(t.price * t.royalty/100 * ta.royaltyper/100 * s.qty + t.advance * ta.royaltyper/100) as TOTAL
     from authors as a 
         inner join titleauthor as ta on ta.au_id = a.au_id 
         inner join titles as t on t.title_id = ta.title_id 
@@ -9,4 +9,16 @@ select
 group by au_lname, au_fname
 order by TOTAL desc
 limit 3;
+
+CREATE TEMPORARY TABLE publications.each_book
+select 
+    a.au_lname LAST_NAME, 
+    a.au_fname FIRST_NAME,
+    sum(distinct(t.price * t.royalty/100 * ta.royaltyper/100)) as TOTAL
+    from authors as a 
+        inner join titleauthor as ta on ta.au_id = a.au_id 
+        inner join titles as t on t.title_id = ta.title_id 
+group by LAST_NAME, FIRST_NAME
+
+
 
