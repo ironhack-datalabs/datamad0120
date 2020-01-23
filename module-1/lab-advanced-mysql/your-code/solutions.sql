e48600164a02a3a4ceb5d8602a9872f9abdb61c4
eduman13 <eduman13@gmail.com>
2020-01-23 10:04:07 +0100

    Subimos la creación de una tabla temporal

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,11 +1,10 @@
 select 
     a.au_lname LAST_NAME,
     a.au_fname FIRST_NAME,
-    sum(t.price * t.royalty/100 * ta.royaltyper/100 * s.qty + t.advance * ta.royaltyper/100) as TOTAL
+    sum(t.price * t.royalty/100 * ta.royaltyper/100 + t.advance * ta.royaltyper/100) as TOTAL
     from authors as a 
         inner join titleauthor as ta on ta.au_id = a.au_id 
         inner join titles as t on t.title_id = ta.title_id 
-        inner join sales as s on s.title_id = t.title_id
 group by au_lname, au_fname
 order by TOTAL desc
 limit 3;
@@ -14,11 +13,20 @@ CREATE TEMPORARY TABLE publications.each_book
 select 
     a.au_lname LAST_NAME, 
     a.au_fname FIRST_NAME,
-    sum(distinct(t.price * t.royalty/100 * ta.royaltyper/100)) as TOTAL
+    t.title_id,
+    ta.royaltyper,
+    t.price * t.royalty/100 * ta.royaltyper/100 as 'EACHBOOK'
     from authors as a 
         inner join titleauthor as ta on ta.au_id = a.au_id 
         inner join titles as t on t.title_id = ta.title_id 
+
+select 
+    LAST_NAME, 
+    FIRST_NAME, 
+    sum(t.advance * e.royaltyper/100 + EACHBOOK) as 'TOTAL'
+    from each_book as e 
+        inner join titles as t on t.title_id = e.title_id
 group by LAST_NAME, FIRST_NAME
-
-
+order by TOTAL desc 
+limit 3;
 
