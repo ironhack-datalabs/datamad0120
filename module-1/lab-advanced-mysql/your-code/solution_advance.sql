76c6cea52481ab1bc6eb487b0572d3162be60a5f
MdeMaic <miguel.ceinos@gmail.com>
2020-01-23 09:30:31 +0100

    CH2 y CH3 DONE

--- module-1/lab-advanced-mysql/your-code/solution_advance.sql
+++ module-1/lab-advanced-mysql/your-code/solution_advance.sql
@@ -1,6 +1,6 @@
 #CHALLENGE 1
 
-/*
+
 CREATE TEMPORARY TABLE `Step1` 
 
 SELECT 
@@ -18,10 +18,10 @@ SELECT
 					ON S.title_id = T.title_id
 
 
-*/
+
 
 #DROP TEMPORARY TABLE IF EXISTS Step2
-/*
+
 CREATE TEMPORARY TABLE Step2
 
 SELECT Title_ID, Author_ID, sum(sales_royalty) AS `SumRoy`
@@ -39,6 +39,71 @@ FROM Step2
 RIGHT JOIN titles as T ON T.title_id = Step2.title_id
 ORDER BY Profit DESC 
 LIMIT 3
-*/
+
+#--------------------------------------------------------------------------------------------------
 
 #CHALLENGE 2
+SELECT Author_ID, (SumRoy + T.advance) AS `Profit`
+
+FROM (
+					SELECT Title_ID, Author_ID, sum(sales_royalty) AS `SumRoy`
+
+					FROM (
+										SELECT 
+
+										T.title_id AS  `Title_ID`,
+										A.au_id AS `Author_ID`, 
+										T.price*S.qty*T.royalty/100*TA.royaltyper/100 AS `sales_royalty`
+
+										FROM titles as T
+										LEFT JOIN titleauthor as TA
+										ON T.title_id = TA.title_id
+										LEFT JOIN authors as A
+										ON TA.au_id = A.au_id
+										RIGHT JOIN sales as S
+										ON S.title_id = T.title_id
+                    
+								) AS Step11
+
+					GROUP BY Title_ID , Author_ID
+
+) AS Step22
+
+RIGHT JOIN titles as T ON T.title_id = Step22.title_id
+ORDER BY Profit DESC 
+LIMIT 3
+
+#--------------------------------------------------------------------------------------------------
+
+#CHALLENGE 3
+
+CREATE TABLE most_profiting_authors
+
+SELECT Author_ID, (SumRoy + T.advance) AS `Profit`
+
+FROM (
+					SELECT Title_ID, Author_ID, sum(sales_royalty) AS `SumRoy`
+
+					FROM (
+										SELECT 
+
+										T.title_id AS  `Title_ID`,
+										A.au_id AS `Author_ID`, 
+										T.price*S.qty*T.royalty/100*TA.royaltyper/100 AS `sales_royalty`
+
+										FROM titles as T
+										LEFT JOIN titleauthor as TA
+										ON T.title_id = TA.title_id
+										LEFT JOIN authors as A
+										ON TA.au_id = A.au_id
+										RIGHT JOIN sales as S
+										ON S.title_id = T.title_id
+                    
+								) AS Step11
+
+					GROUP BY Title_ID , Author_ID
+
+) AS Step22
+
+RIGHT JOIN titles as T ON T.title_id = Step22.title_id
+ORDER BY Profit DESC 
