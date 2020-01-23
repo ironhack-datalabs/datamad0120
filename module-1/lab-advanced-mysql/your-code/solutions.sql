c3a60f26c2298fb3ca7749b1988d4ea521fc5bd3
eduman13 <eduman13@gmail.com>
2020-01-23 10:10:54 +0100

    Subimos la creacion de la tabla Eduardo@DESKTOP-8REJJ07 MINGW64 ~

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -9,6 +9,8 @@ group by au_lname, au_fname
 order by TOTAL desc
 limit 3;
 
+-------
+
 CREATE TEMPORARY TABLE publications.each_book
 select 
     a.au_lname LAST_NAME, 
@@ -18,7 +20,7 @@ select
     t.price * t.royalty/100 * ta.royaltyper/100 as 'EACHBOOK'
     from authors as a 
         inner join titleauthor as ta on ta.au_id = a.au_id 
-        inner join titles as t on t.title_id = ta.title_id 
+        inner join titles as t on t.title_id = ta.title_id;
 
 select 
     LAST_NAME, 
@@ -29,4 +31,18 @@ select
 group by LAST_NAME, FIRST_NAME
 order by TOTAL desc 
 limit 3;
+
+-------
+
+create table most_profiting_authors as
+(
+select
+    a.au_id author_id,
+    sum(t.price * t.royalty/100 * ta.royaltyper/100 + t.advance * ta.royaltyper/100) as profits
+    from authors as a 
+        inner join titleauthor as ta on ta.au_id = a.au_id 
+        inner join titles as t on t.title_id = ta.title_id 
+group by author_id
+order by profits desc 
+);
 
