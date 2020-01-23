373f72f266c5a36956489b3a708177e4dda0e023
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-01-23 09:39:15 +0100

    Dooooooneeeee 🔥💪🏻

--- module-1/lab-advanced-mysql/your-code/solucions_advanced.sql
+++ module-1/lab-advanced-mysql/your-code/solucions_advanced.sql
@@ -48,5 +48,26 @@ SELECT au_id, sum(titles.advance + sum_royalty) as profit
 	ON paso2.title_id = titles.title_id
 	GROUP BY au_id;
 
+-------CHALLENGE 3--------
+
+CREATE TABLE `most_profiting_authors` (SELECT au_id, sum(titles.advance + sum_royalty) as profit
+		FROM (
+			SELECT au_id, title_id, sum(sales_royalty) as sum_royalty
+			FROM (
+					SELECT titleauthor.au_id, titleauthor.title_id, titleauthor.royaltyper, 
+					titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty
+					FROM titleauthor
+					INNER JOIN publications.titles
+					ON titleauthor.title_id = titles.title_id
+					INNER JOIN publications.sales
+					ON sales.title_id = titleauthor.title_id
+					) as paso1
+			GROUP BY au_id, title_id) as paso2
+	INNER JOIN publications.titles 
+	ON paso2.title_id = titles.title_id
+	GROUP BY au_id
+)
+
+
 
 */
