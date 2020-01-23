b754502ba81628ad50cc1c2d4d3ca213f3611ded
Alex <correovidal@outlook.com>
2020-01-23 01:15:35 +0100

    Ejercicio en progreso V4

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,24 +1,69 @@
-/*
+/* EJERCICIO 1 */
+
+
 CREATE TEMPORARY TABLE publications.Paso1
 
 SELECT
 	titles.title_id AS TitleID,
 	authors.au_id AS AuthorsID,
-    	titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 AS Sales_Royalties
-
-FROM authors
+    titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 AS Sales_Royalties
+    FROM authors
     
     INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
     INNER JOIN titles ON titles.title_id=titleauthor.title_id
     INNER JOIN sales ON sales.title_id=titles.title_id;
-*/    
 
-
-/*
-
-PD EXTRA --> PORQUE NO FUNCIONA DE ESTA MANERA, ¿NO ES IGUAL QUE INSERTAR UNA TABLA TEMPORAL ANTERIOR?
 
 CREATE TEMPORARY TABLE publications.Paso2
+
+SELECT 
+	AuthorsID,
+	TitleID,
+	SUM(Sales_Royalties) as Suma
+    
+FROM Paso1
+GROUP BY AuthorsID, TitleID;
+
+
+SELECT
+
+	AuthorsID,
+	ROUND(SUM(Suma + titles.advance)) as Profits
+    
+FROM Paso2
+	INNER JOIN publications.titles ON titles.title_id=Paso2.TitleID
+    
+    
+GROUP BY AuthorsID;
+
+DROP TEMPORARY TABLE Paso1,Paso2
+
+
+
+
+
+
+
+
+
+/* EJERCICIO 2 */
+
+SELECT
+	titles.title_id AS TitleID,
+	authors.au_id AS AuthorsID,
+    titles.price*sales.qty*titles.royalty/100*titleauthor.royaltyper/100 AS Sales_Royalties
+    FROM authors
+    
+    INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
+    INNER JOIN titles ON titles.title_id=titleauthor.title_id
+    INNER JOIN sales ON sales.title_id=titles.title_id;
+
+
+
+
+
+
+/* ¿PORQUE NO FUNCIONA DE ESTA MANERA? ¿MALA SINTAXIS? */
 
 SELECT 
 	AuthorsID,
@@ -34,23 +79,8 @@ FROM(
     
     INNER JOIN titleauthor ON titleauthor.au_id=authors.au_id
     INNER JOIN titles ON titles.title_id=titleauthor.title_id
-    INNER JOIN sales ON sales.title_id=titles.title_id;
-	
+    INNER JOIN sales ON sales.title_id=titles.title_id
+
 ) as Paso1
+
 GROUP BY AuthorsID, TitleID;
-
-*/
-
-/*
-CREATE TEMPORARY TABLE publications.Paso2
-
-SELECT 
-	AuthorsID,
-	TitleID,
-	SUM(Sales_Royalties) as Suma
-    
-FROM Paso1
-GROUP BY AuthorsID, TitleID;
-
-*/
-
