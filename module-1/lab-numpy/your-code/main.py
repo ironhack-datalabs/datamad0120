8fc49c4d5764c01ef2c9a0e341f856015b2804a1
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-01-28 20:18:47 +0100

    Terminando bonus odio numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,70 +1,75 @@
 #1. Import the NUMPY package under the name np.
 import numpy as np
 
-
 #2. Print the NUMPY version and the configuration.
-
-
+print(np.__version__)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2,3,5))
 
-
+#np.random.rand(2,3,5) --> segunda forma
+#np.random.random_sample((2,3,5)) --> tercera forma
 #4. Print a.
-
-
+print("Array a")
+print (a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
-#Assign the array to variable "b"
-
-
+#Assign the array to variable "b" 
+b = np.ones((5,2,3))
 
 #6. Print b.
-
-
+print("Array b")
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.size == b.size)
+print(a.size)
+#Ambos tienen 30
 
 
 #8. Are you able to add a and b? Why or why not?
-
-
+#np.add(a,b)
+#ValueError: operands could not be broadcast together with shapes (2,3,5) (5,2,3) 
+#No pueden unirse porque no tienen la misma forma, aunque tengan el mismo tamaño.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+c = b.transpose(1,2,0)
+print("Array b tras np.reshape")
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+d = np.add(c,a)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+#Al hacer transpose() tienen la misma forma, por eso se pueden sumar. Pero hay un error en el ejercicio 5
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
-
+e = a * c
+print("Imprimo a * c")
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
-
-
+print(e == a)
+# sí
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
+print(f"Máximo {d_max}, mínimo {d_min}, media {d_mean}")
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
-
+f = np.empty ((2,3,5))
+print(f)
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
@@ -75,9 +80,21 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
-
-
+for a in range (f.shape[0]):
+        for b in range (f.shape[1]):
+                for c in range (f.shape[2]):
+                        if  d_min < d[a][b][c] < d_mean:
+                                f[a][b][c] = 25
+                        elif d_mean < d[a][b][c] < d_max:
+                                f[a][b][c] = 75
+                        elif d[a][b][c] == d_mean:
+                                f[a][b][c] = 50
+                        elif d[a][b][c] == d_min:
+                                f[a][b][c] = 0
+                        elif d[a][b][c] == d_max:
+                                f[a][b][c] = 100
+                   
+print(f)
 """
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
@@ -111,4 +128,20 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
-"""
+# """
+f = f.astype('str')
+for a in range (f.shape[0]):
+        for b in range (f.shape[1]):
+                for c in range (f.shape[2]):
+                        if  d_mean < d[a][b][c] < d_max:
+                                f[a][b][c] = 'D'
+                        elif d_min < d[a][b][c] < d_mean:
+                                f[a][b][c] = 'B'
+                        elif d[a][b][c] == d_mean:
+                                f[a][b][c] = 'C'
+                        elif d[a][b][c] == d_min:
+                                f[a][b][c] = 'A'
+                        elif d[a][b][c] == d_max:
+                                f[a][b][c] = 'E'
+
+print(f)
