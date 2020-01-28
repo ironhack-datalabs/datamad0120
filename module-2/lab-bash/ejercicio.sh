6b7c238742e85d22e5041f4093b0cd711e1e37ea
Fernando Costa <fernandocosta@Fernandos-MacBook-Pro.local>
2020-01-28 16:11:13 +0100

    50%

---
+++ module-2/lab-bash/ejercicio.sh
@@ -0,0 +1,22 @@
+echo Hello World
+mkdir new_dir
+rmdir new_dir
+mv sed.txt /Users/fernandocosta/datamad0120/module-2/lab-bash/lorem-copy
+mv at.txt /Users/fernandocosta/datamad0120/module-2/lab-bash/lorem-copy; mv lorem.txt /Users/fernandocosta/datamad0120/module-2/lab-bash/lorem-copy
+cat sed.txt
+cat at.txt ; cat lorem.txt
+head -3 sed.txt
+echo "Homo homini lupus." >> sed.txt
+tail -3 sed.txt
+#Sustituye todas las apariciones de et por ET del archivo at.txt dentro de la carpeta lorem-copy. Deberás usar sed.
+whoami
+pwd
+#Lista los archivos que terminan por .txt en la carpeta lorem.
+find lorem
+#Lista los archivos que terminan por .txt en la carpeta lorem.
+#Cuenta el número de líneas que tiene el archivo sed.txt dentro de la carpeta lorem.
+#Cuenta el número de archivos que empiezan por lorem que están en este directorio y en directorios internos.
+#Encuentra todas las apariciones de et en at.txt dentro de la carpeta lorem.
+#Cuenta el número de apariciones del string et en at.txt dentro de la carpeta lorem.
+#Cuenta el número de apariciones del string et en todos los archivos del directorio lorem-copy.
+
