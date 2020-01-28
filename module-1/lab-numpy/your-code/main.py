297d7a8267cc31a6880df215430b8efd2c98b8a9
x22a <xabiergdvz@gmail.com>
2020-01-28 19:48:08 +0100

    Locura final done, thx Patri

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,79 +1,106 @@
 #1. Import the NUMPY package under the name np.
 
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.rand(2, 3, 5)
+#a = np.random.randint(10, size = (2, 3, 5))
+#a = np.random.choice(10, size = (2, 3, 5))
 
 #4. Print a.
 
-
+print('a = ', a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5, 2, 3))
 
 #6. Print b.
 
-
+print('b = ', b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(np.size(a) == np.size(b))
 
 
 #8. Are you able to add a and b? Why or why not?
-
-
+# No, they have to have the same shame
+#print(a + b)
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = b.transpose((1,2,0))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print('a = ', a)
+print('d = ', d)
 
-
-
+# The values of d are the values of a plus 1, because c is a all 1 matrix
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
 
+print(e == a)
 
-
+# They're equal because we are multiplying the values by 1
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty(shape = [2,3,5])
+print(f)
 
 
 
-"""
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
+'''
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
-"""
+'''
+newZ = [[[]for j in range(3)] for i in range(2)]
+for elem in range(2):
+  for elem2 in range(3):
+    for elem3 in range(5):
+      dFin = (d[elem][elem2][elem3])
+      if dFin > d_min and dFin < d_mean:
+        newZ[elem][elem2].append(25)
+      elif dFin > d_min and dFin < d_max:
+        newZ[elem][elem2].append(75)
+      elif dFin == d_mean:
+        newZ[elem][elem2].append(50)
+      elif dFin == d_min:
+        newZ[elem][elem2].append(0)
+      elif dFin == d_max:
+        newZ[elem][elem2].append(100)
+
+f = np.asarray(newZ)
 
 
 
@@ -89,6 +116,10 @@ array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.79129243, 1.74983003, 1.96028037, 1.85166831, 1.65450881],
         [1.18068344, 1.9587381 , 1.00656599, 1.93402165, 1.73514584]]])
 
+"""
+print(d)
+print(f)
+"""
 Your f should be:
 array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  25.,  25.,  25.],
@@ -112,3 +143,21 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+newZ = [[[]for j in range(3)] for i in range(2)]
+for elem in range(2):
+  for elem2 in range(3):
+    for elem3 in range(5):
+      dFin = (d[elem][elem2][elem3])
+      if dFin > d_min and dFin < d_mean:
+        newZ[elem][elem2].append('B')
+      elif dFin > d_min and dFin < d_max:
+        newZ[elem][elem2].append('D')
+      elif dFin == d_mean:
+        newZ[elem][elem2].append('C')
+      elif dFin == d_min:
+        newZ[elem][elem2].append('A')
+      elif dFin == d_max:
+        newZ[elem][elem2].append('E')
+
+newF = np.asarray(newZ)
+print(newF)
