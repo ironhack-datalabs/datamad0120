53df6dd313b846645c44674677102ddc10581766
guillermo.martinez <guillermomartinezcastro@gmail.com>
2020-01-28 20:23:26 +0100

    [lab-numpy] Guillermo Martínez

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,72 +1,91 @@
-#1. Import the NUMPY package under the name np.
+# 1. Import the NUMPY package under the name np.
+
+import numpy as np
 
 
+# 2. Print the NUMPY version and the configuration.
 
-#2. Print the NUMPY version and the configuration.
+print(np.version.version)
 
-
-
-#3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
+# 3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2, 3, 5))
 
 
-#4. Print a.
+# 4. Print a.
+
+print(a)
+
+# 5. Create a 5x2x3 3-dimensional array with all values equaling 1.
+# Assign the array to variable "b"
+
+b = np.ones((5, 2, 3))
+
+# 6. Print b.
+
+print(b)
+
+# 7. Do a and b have the same size? How do you prove that in Python code?
+
+print(a.size == b.size)
+
+# 8. Are you able to add a and b? Why or why not?
+
+try:
+    c = np.add(a, b)
+
+except Exception as error:
+    print(
+        f"We cant add the arrays because they have different shapes, a has this shape{a.shape}, and b has this shape {b.shape}")
+
+# 9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+
+c = np.transpose(b, (1, 2, 0))
+
+print(c)
+
+# 10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
 
+d = np.add(a, c)
 
-#5. Create a 5x2x3 3-dimensional array with all values equaling 1.
-#Assign the array to variable "b"
-
-
-
-#6. Print b.
+print(
+    f"Now we can add the arrays because they have same shape, a has this shape{a.shape}, and c has this shape {c.shape}")
 
 
+# 11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-#7. Do a and b have the same size? How do you prove that in Python code?
+print(a, d)
 
+# The values of "d" are the values of "a" + 1, because "c" is a matrix with all the values as 1
 
+# 12. Multiply a and c. Assign the result to e.
 
+e = a * c
 
-#8. Are you able to add a and b? Why or why not?
+# 13. Does e equal to a? Why or why not?
 
+print(e == a)
 
+# e == a because e is the result of multiply the values of a * 1
 
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+# 14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
+# 15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
-
-#11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
-
-#12. Multiply a and c. Assign the result to e.
-
-
-
-#13. Does e equal to a? Why or why not?
-
-
-
-
-#14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
-
-#15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
+f = np.empty([2, 3, 5])
+print(f)
+print(d_max)
+print(d_min)
+print(d_mean)
 
 """
-#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
+# 16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
@@ -75,11 +94,24 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+for elem in range(2):
+    for elem2 in range(3):
+        for elem3 in range(5):
+            dFin = (d[elem][elem2][elem3])
+            if dFin > d_min and dFin < d_mean:
+                f[elem][elem2][elem3] = 25
+            elif dFin > d_min and dFin < d_max:
+                f[elem][elem2][elem3] = 75
+            elif dFin == d_mean:
+                f[elem][elem2][elem3] = 50
+            elif dFin == d_min:
+                f[elem][elem2][elem3] = 0
+            elif dFin == d_max:
+                f[elem][elem2][elem3] = 100
 
 
 """
-#17. Print d and f. Do you have your expected f?
+# 17. Print d and f. Do you have your expected f?
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
@@ -98,10 +130,11 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print(f)
+print(d)
 
 """
-#18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
+# 18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
 array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'B',  'B',  'B'],
@@ -112,3 +145,22 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+f = np.empty(shape=[2, 3, 5], dtype='str')
+
+for elem in range(2):
+    for elem2 in range(3):
+        for elem3 in range(5):
+            dFin = (d[elem][elem2][elem3])
+            if dFin > d_min and dFin < d_mean:
+                f[elem][elem2][elem3] = 'B'
+            elif dFin > d_min and dFin < d_max:
+                f[elem][elem2][elem3] = 'D'
+            elif dFin == d_mean:
+                f[elem][elem2][elem3] = 'C'
+            elif dFin == d_min:
+                f[elem][elem2][elem3] = 'A'
+            elif dFin == d_max:
+                f[elem][elem2][elem3] = 'E'
+
+print(f)
