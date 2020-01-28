9037ec214ee1792a8c29078be0005052568b47f4
Fernando Costa <fernandocosta@Fernandos-MacBook-Pro.local>
2020-01-28 17:21:23 +0100

    100%

--- module-2/lab-bash/ejercicio.sh
+++ module-2/lab-bash/ejercicio.sh
@@ -20,15 +20,18 @@ head -3 sed.txt
 echo "Homo homini lupus." >> sed.txt
 #Visualiza las últimas 3 líneas del archivo sed.txt dentro de la carpeta lorem-copy. Deberías ver ahora Homo homini lupus..
 tail -3 sed.txt
-!!!#Sustituye todas las apariciones de et por ET del archivo at.txt dentro de la carpeta lorem-copy. Deberás usar sed.
+#Sustituye todas las apariciones de et por ET del archivo at.txt dentro de la carpeta lorem-copy. Deberás usar sed.
+sed 's/et/ET' /Users/fernandocosta/datamad0120/module-2/lab-bash/at.txt
 #Encuentra al usuario activo en el sistema.
 whoami
 #Encuentra dónde estás en tu sistema de ficheros.
 pwd
-!!!!#Lista los archivos que terminan por .txt en la carpeta lorem.
+#Lista los archivos que terminan por .txt en la carpeta lorem.
+find ./lorem -type f -name "*.txt"
 #Cuenta el número de líneas que tiene el archivo sed.txt dentro de la carpeta lorem.
 cat sed.txt | wc -l
-!!!!#Cuenta el número de archivos que empiezan por lorem que están en este directorio y en directorios internos.
+#Cuenta el número de archivos que empiezan por lorem que están en este directorio y en directorios internos.
+find lorem | wc -l
 #Encuentra todas las apariciones de et en at.txt dentro de la carpeta lorem.
 grep -r "et" /Users/fernandocosta/datamad0120/module-2/lab-bash/lorem
 #Cuenta el número de apariciones del string et en at.txt dentro de la carpeta lorem.
