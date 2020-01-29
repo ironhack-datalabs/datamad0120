5348d4eb3dda06c6179e61f1b6aa273bd9e844a8
fabianlojo <59798704+fabianlojo@users.noreply.github.com>
2020-01-29 22:42:29 +0100

    bonus lab bash comenzado

---
+++ module-2/lab-bash/name.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+echo "Wake up, Data TA"
+sleep 3
+clear
+echo "The Matrix has you..."
+sleep 3
+clear
+echo "Follow the white rabbit."
+sleep 3
+clear
+echo "Knock, knock, TA."
+sleep 3
+clear
+echo "Just Kidding ^^ continue with your lab review"
+sleep 3
+clear 
+echo "how is your day so far?"
+read ans
+
+clear
+echo "you typed ($ans)"
+sleep 2
+clear
+echo "hope it improves for better ;)"
+sleep 3
+bash name2.sh
---
+++ module-2/lab-bash/name2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="Fabian"
+
+echo $name
+
+for i in $(ls):
+do
+	echo $i
+done
+
+echo "-Just got here :( please help me understand how to do the rest"
