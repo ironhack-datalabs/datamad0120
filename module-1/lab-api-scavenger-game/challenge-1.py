5930680d860387f40eebed6401d1b574c8a4aab8
lauravillalba <lvillalba@live.com>
2020-01-29 21:56:36 +0100

    locuras con los commits

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,12 +1,12 @@
 import json
 import requests
 import os
+import re
 from dotenv import load_dotenv
 load_dotenv('your-code/.env')
 
 def requestGithub(endpoint):
     token = os.getenv("GITHUB_APIKEY")
-    print (token)
     if not token:
         raise ValueError("Necesitas un GITHUB_APIKEY token")
     
@@ -26,4 +26,16 @@ requestGithub("/repos/ironhack-datalabs/datamad0120")
 
 data=requestGithub("/repos/ironhack-datalabs/datamad0120/forks")
 
+'''
+Compruebo si el total de fork, coincide con el que se muestra en la página.
+'''
+
 print(len(data))
+
+list_language=[]
+for e in data:   
+    if "language" in e:
+        if e["language"] not in list_language:
+            list_language.append(e["language"])
+
+print(list_language)
